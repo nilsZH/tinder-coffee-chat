2bbeb7ce3f380b414023866e71a4934b2b725938
zum Hebel <Hebel.Nils@bcg.com>
2020-10-23 22:19:11 +0200

    Implemented Auth incl smaple login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Route, Switch, Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Login from './login/Login';
 import Main from './main/Main';
 import Profile from './profile/Profile';
 import Matches from './matches/Matches';
+import PrivateRoute from './PrivateRoute';
+import { AuthContext, useAuth } from "./context/auth";
 import './App.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,31 +25,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App() {
-  const name = 'John Doe'
-  const isAuthenticated = true
+function App(props) {
   const classes = useStyles();
+  const exisitinTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(exisitinTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
+  function logOut() {
+    setAuthTokens();
+  }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Coffee Time
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Route path="/login" component={Login} />
-      {
-      isAuthenticated ? 
-      <>
-      <Route exact={true} path="/" component={Main} />
-      <Route exact={true} path="/profile" component={Profile} />
-      <Route exact={true} path="/matches" component={Matches} />
-      </> : <Redirect to="/login" />
-      }
-    </div>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Coffee Time
+            </Typography>
+            { authTokens ? 
+                <Button color="inherit" onClick={logOut}>Logout</Button> :
+                <Button color="inherit">Login</Button>
+            }         
+          </Toolbar>
+        </AppBar>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact={true} path="/" component={Main} />
+        <PrivateRoute exact={true} path="/profile" component={Profile} />
+        <PrivateRoute exact={true} path="/matches" component={Matches} />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,69 +1,71 @@
-import React, { Component } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { Container, Button, Grid, TextField } from '@material-ui/core';
+import { useAuth } from "../context/auth";
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
 import './Login.css';
 
-class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      username:'',
-      password: ''
-    }
+function Login() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthTokens } = useAuth();
 
-    this.handleUsername = this.handleUsername.bind(this);
-    this.handlePassword = this.handlePassword.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function postLogin() {
+//    axios.post("http://www.dummy.restapiexample.com/create", {
+//      userName,
+//      password
+//    }).then(result => {
+//      if (result.status !== 200) {
+//        setAuthTokens(jwt.sign({ foo: 'bar' }, 'shhhhh'));
+//        setLoggedIn(true);
+//      } else {
+//        setIsError(true);
+//      }
+//    }).catch(e => {
+//      setIsError(true);
+//    });
+    setAuthTokens(jwt.sign({ foo: 'bar' }, 'shhhhh'));
+    setLoggedIn(true);    
   }
 
-  handleUsername(event) {
-    this.setState({username: event.target.value});
+  if (isLoggedIn) {
+    return <Redirect to="/" />
   }
 
-  handlePassword(event) {
-    this.setState({password: event.target.value});
-  }
-
-  handleSubmit(event) {
-    let login = 'test';
-    if (this.state.password === login) {
-      this.setState({isAuthenticated: true})
-      this.history.push('/')
-    }
-    event.preventDefault();
-  } 
-  
-  nextPath(path) {
-    this.props.history.push(path);
-  }
-
-  render() {
-    return (
-        <Container className="container" maxWidth="xs">
-          <form>
-            <Grid container spacing={3}>
+  return (
+    <Container className="container" maxWidth="xs">
+      <img src="../public/coffee.png" />
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
               <Grid item xs={12}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField fullWidth label="Username" name="username" size="small" variant="outlined" />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField fullWidth label="Password" name="password" size="small" type="password" variant="outlined" />
-                  </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Button color="secondary" fullWidth type="submit" variant="contained">
-                      <Link to='/'>Log in</Link>
-                    </Button>
-                  </Grid>
-                </Grid>
+                <TextField fullWidth label="Username" name="username" size="small" variant="outlined" onChange={e => {
+                  setUserName(e.target.value);
+                }}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Password" name="password" size="small" type="password" variant="outlined" onChange={e => {
+                  setPassword(e.target.value);
+                }} />
               </Grid>
             </Grid>
-          </form>
-        </Container>
-    );
-  }
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Button color="secondary" fullWidth variant="contained" onClick={postLogin}>
+                  Log In
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+      { isError &&<p>The username or password provided were incorrect!</p>}
+    </Container>
+  );
 }
 
 export default Login;
